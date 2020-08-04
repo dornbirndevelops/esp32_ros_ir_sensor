b9f358b13a13d673c6ed0b5ec3715bd6e1468d1d
dornbirndevelops <dornbirndevelops@gmail.com>
2020-08-04 13:02:22 +0200

    calibration done

--- main/sensor_node.cpp
+++ main/sensor_node.cpp
@@ -8,32 +8,52 @@
 #include <algorithm>
 #include "sensor_node.h"
 
+#define CALIBRATE 0
 #define NO_OF_SAMPLES 65
-#define A 0.00003047956374f
-#define B 0.004288992178f
-#define K 1.5f
+#define RING 6
+
+typedef struct {
+  std::string frame;
+  adc1_channel_t pin;
+  float A;
+  float B;
+  float K;
+} sensor_t;
+
+/*
+ADC1 channel 0 is GPIO36
+ADC1 channel 1 is GPIO37
+ADC1 channel 2 is GPIO38
+ADC1 channel 3 is GPIO39
+ADC1 channel 4 is GPIO32
+ADC1 channel 5 is GPIO33
+ADC1 channel 6 is GPIO34
+ADC1 channel 7 is GPIO35
+*/
+
+// {"ring_4_back", ADC1_CHANNEL_7, 0.0000315383707f, 0.002978491566f, 1.5f}, best sensor so far
+
+std::vector<sensor_t> sensors = {
+#if RING == 2
+  {"ring_2_front", ADC1_CHANNEL_6, 0.00003096556491f, 0.004266254642f, 2.1f},
+  {"ring_2_left", ADC1_CHANNEL_5, 0.00003276791572f, 0.00284869739f, 0.9f},
+  {"ring_2_back", ADC1_CHANNEL_4, 0.00003344496413f, 0.002923084849f, 0.9f},
+  {"ring_2_right", ADC1_CHANNEL_7, 0.00003138671844f, 0.004107259821f, 1.5f},
+#elif RING == 4
+  {"ring_4_front", ADC1_CHANNEL_5, 0.00003043101771f, 0.002900329139f, 1.5f},
+  {"ring_4_left", ADC1_CHANNEL_4, 0.00003110156825f, 0.002753638173f, 1.5f},
+  {"ring_4_back", ADC1_CHANNEL_7, 0.0000315383707f, 0.002978491566f, 1.5f},
+  {"ring_4_right", ADC1_CHANNEL_6, 0.00003096265037f, 0.002803218224f, 1.5f},
+#elif RING == 6
+  {"ring_6_front_right", ADC1_CHANNEL_6, 0.00002897696896f, 0.003815309638f, 1.5f},
+  {"ring_6_front_left", ADC1_CHANNEL_7, 0.0000282914602f, 0.002773656281f, 2.0f},
+#endif
+};
 
 ros::NodeHandle nh;
 
 sensor_msgs::Range range_msg;
 ros::Publisher range_publisher("ir_data", &range_msg);
-
-std::vector<std::pair<std::string, adc1_channel_t> > sensors = {
-  std::make_pair("bar", ADC1_CHANNEL_6)
-};
-
-float read_range(adc1_channel_t channel)
-{
-  // Multisampling with median
-  int raw_samples[NO_OF_SAMPLES];
-  for (int i = 0; i < NO_OF_SAMPLES; i++) {
-    raw_samples[i] = adc1_get_raw(channel);
-  }
-  std::sort(raw_samples, raw_samples+NO_OF_SAMPLES);
-  auto adc_value = raw_samples[NO_OF_SAMPLES/2];
-
-  return (1 / (A * adc_value + B) - K) / 100;
-}
 
 void rosserial_setup()
 {
@@ -50,7 +70,7 @@ void rosserial_setup()
   // Initialize ADC
   adc1_config_width(ADC_WIDTH_BIT_12);
   for(auto i = sensors.begin(); i < sensors.end(); i++) {
-    adc1_config_channel_atten(i->second,ADC_ATTEN_DB_11);
+    adc1_config_channel_atten(i->pin,ADC_ATTEN_DB_11);
   }
 }
 
@@ -58,8 +78,19 @@ void rosserial_publish()
 {
   for(auto i = sensors.begin(); i < sensors.end(); i++) {
     // Fill dynamic message data
-    range_msg.header.frame_id = i->first.c_str();
-    range_msg.range = read_range(i->second);
+    // Multisampling with median
+    int raw_samples[NO_OF_SAMPLES];
+    for (int j = 0; j < NO_OF_SAMPLES; j++) {
+      raw_samples[j] = adc1_get_raw(i->pin);
+    }
+    std::sort(raw_samples, raw_samples+NO_OF_SAMPLES);
+    auto adc_value = raw_samples[NO_OF_SAMPLES/2];
+    #if CALIBRATE == 1
+    range_msg.range = adc_value;
+    #else
+    range_msg.range = (1 / (i->A * adc_value + i->B) - i->K) / 100;
+    #endif
+    range_msg.header.frame_id = i->frame.c_str();
     range_msg.header.stamp = nh.now();
     
     // Publish message
